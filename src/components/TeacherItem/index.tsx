0945665038f16a8d3a6a0216b22088ae900794fe
root <root@PORTUGA.localdomain>
2020-08-09 20:56:08 -0300

    Aula 3 Parte 2

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,36 +1,58 @@
-import React from 'react';
+import React from "react";
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
-import './style.css'
+import "./style.css";
+import api from "../../services/api";
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post("connections", {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/68035365?s=460&v=4" alt="Diego Fernandes" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Diego Fernandes</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Texto engraçado do cara que fez as telas
-    <br /><br />
-    KKKKKKKKKKK
-  </p>
+      <p>{teacher.bio}</p>
       <footer>
         <p>
           Preco/hora
-      <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          onClick={createNewConnection}
+          target="_blank"
+          href={`https://wa.me/${teacher.whatsapp}`}
+          rel="noopener noreferrer"
+          type="button"
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
-      Entrar em contato
-    </button>
+          Entrar em contato
+        </a>
       </footer>
     </article>
-  )
-}
+  );
+};
 
-export default TeacherItem
+export default TeacherItem;
